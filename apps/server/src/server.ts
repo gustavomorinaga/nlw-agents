f034fcb3891af5ff5ec3e1e06198c8082da87fc6
Gustavo Morinaga <me@gustavomorinaga.dev>
2025-07-09 13:35:49 -0300

    feat: db setup and seed

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,11 +1,12 @@
 import fastifyCors from '@fastify/cors';
 import fastify from 'fastify';
-import type { ZodTypeProvider } from 'fastify-type-provider-zod';
 import {
 	serializerCompiler,
 	validatorCompiler,
+	type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
 import { env } from './env.ts';
+import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -17,6 +18,8 @@ app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 app.get('/health', () => 'OK');
+
+app.register(getRoomsRoute);
 
 app.listen({ port: env.PORT }).then(() => {
 	// console.log(`Server is running on http://localhost:${env.PORT}`);
